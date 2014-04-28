7a90d2906b3a8656fb6eafd38724d1a2561ba53e
Oleksiy Protas <elfy.ua@gmail.com>
2014-04-28 18:46:40 +0300

    Basic streaming framework

--- YaTWC.c
+++ YaTWC.c
@@ -3,8 +3,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <limits.h>
+#include <limits.h>             /* for NGROUPS_MAX */
 #include <linux/videodev2.h>
+#include <string.h>             /* for memset() */
+#include <stdlib.h>
+#include <errno.h>
 
 /* Check that the camera device exists and is a-priori usable for us */
 int checkcam(const char *path)
@@ -45,6 +48,7 @@ int closecam(int fd)
     return close(fd);
 }
 
+/* TODO: deal with errno */
 void listcams()
 {
     int i,fd;
@@ -64,20 +68,93 @@ void listcams()
                 printf("Error opening %s.\n", namebuf);
                 goto deviceclosed;
             }
-                
+            
+            /* Querying and displaying the capabilities */
             if ( ioctl(fd, VIDIOC_QUERYCAP, &caps) == -1 )
             {
-                printf("Error doing ioctl on %s.\n", namebuf);
+                printf("Error querying capabilities on %s.\n", namebuf);
                 goto deviceopen;
             }
             printf("Webcam detected: %s at %s, driver: %s\n", caps.card, namebuf, caps.driver);
             
+            /* Let's check if it's a camera at all */
             if ( (caps.capabilities & (V4L2_CAP_VIDEO_CAPTURE|V4L2_CAP_VIDEO_CAPTURE_MPLANE)) == 0 )
-                printf("\tWebcam does not support video capture.");
-            else
             {
+                printf("Webcam does not support video capture.");
+                goto deviceopen;
             }
+            
+            /* Streaming IO through a user prointer */
+            /* TODO:  implement readwrite fallback */
+            if ( caps.capabilities & V4L2_CAP_STREAMING )
+            {
+                /* TODO: evaluate mmap */
+                /* Actual buffers */
+                size_t buflen;
+                void *databuf;
+                struct v4l2_requestbuffers reqbuf;
+                struct v4l2_format fmt;
+                struct v4l2_buffer buf;
+                
+                /* Requesting native format info */
+                fmt.type=V4L2_BUF_TYPE_VIDEO_CAPTURE;
+                if ( ioctl(fd, VIDIOC_G_FMT, &fmt) == -1 )
+                {
+                    printf("Can not request format information on %s.\n", namebuf);
+                    goto deviceopen;
+                }
+                char *fmtpxfmt=(char*)&fmt.fmt.pix.pixelformat;
+                char pixelformat[5] = { fmtpxfmt[0], fmtpxfmt[1], fmtpxfmt[2], fmtpxfmt[3], '\0' };
+                printf("Webcam suggestested format: %dx%d %s.\n", fmt.fmt.pix.width, fmt.fmt.pix.height, pixelformat);
+                
+                /* Allocating the buffer */
+                buflen=fmt.fmt.pix.sizeimage;
+                databuf=(void*)malloc(buflen);
+                
+                /* Requesting the buffer through a user pointer */
+                memset(&reqbuf, 0, sizeof (reqbuf));
+                reqbuf.type=V4L2_BUF_TYPE_VIDEO_CAPTURE;
+                reqbuf.memory=V4L2_MEMORY_USERPTR;
+                reqbuf.count=1;
+               
+                if ( ioctl(fd, VIDIOC_REQBUFS, &reqbuf) == -1 )
+                {
+                    printf("Error requesting buffers on %s.\n", namebuf);
+                    goto buffersallocated;
+                }            
+                
+                /* Binding the buffer */
+                memset(&buf, 0, sizeof(buf));
+                buf.type=V4L2_BUF_TYPE_VIDEO_CAPTURE;
+                buf.memory=V4L2_MEMORY_USERPTR;
+                buf.index=0;
+                buf.m.userptr=(unsigned long)databuf;
+                buf.length=buflen;
 
+                if ( ioctl(fd, VIDIOC_QBUF, &buf) == -1 )
+                {
+                    printf("Can not bind the buffer on %s.\n", namebuf);
+                    perror("");
+                    goto buffersallocated;
+                }
+                
+                /* Starting the stream */
+                if ( ioctl(fd, VIDIOC_STREAMON, &fmt.type) == -1 )
+                {
+                    printf("Can not start the stream on %s.\n", namebuf);
+                    goto buffersallocated;
+                }
+                
+                /* Stopping the stream */
+                if ( ioctl(fd, VIDIOC_STREAMOFF, &fmt.type) == -1 )
+                {
+                    printf("Can not stop the stream on %s.\n", namebuf);
+                    goto buffersallocated;
+                }
+                
+                buffersallocated:
+                free(databuf);
+            }
             
             deviceopen:
             if ( close(fd) == -1 )
